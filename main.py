4a3f40dbfcada7917a6ac55e32f8861f9680a440
raphavtorres <raphaeltorresprofissional@gmail.com>
2023-09-27 16:57:43 -0300

    update

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
-from fastapi import FastAPI
+import json
+import pathlib
+from typing import List, Union
+
+from fastapi import FastAPI, Response
+
+from models import Book
 
 app = FastAPI()
 
-users = []
+data = []
 
-@app.get('/users')
-async def get_user():
-  return users
+@app.on_event('startup')
+async def startup_event():
+  datapath = pathlib.Path() / 'data' / 'books.json'
+  with open(datapath, 'r') as f:
+    books = json.load(f)
+    for book in books:
+      data.append(Book(**book).dict())
 
-@app.post('/users')
-async def create_user(user):
-  users.append(user)
-  return {'message': 'Hello World!'}
+
+# GET ALL BOOKS
+@app.get('/api/v1/books/', response_model=List[Book])
+def get_all_books():
+  return data
+
+
+# GET BOOK BY ID
+@app.get('/api/v1/books/{book_id}', response_model=Union[Book, str])
+def books(book_id: int, response: Response):
+  # find book by id or return None
+  book = None
+  for b in data:
+    if b['id'] == book_id:
+      book = b
+      break
+
+  if book is None:
+    response.status_code = 404
+    return "Book not found"
+  return book
+
+
+# CREATE NEW BOOK
+@app.post('/api/v1/books/', response_model=Book, status_code=201)
+def add_book(book: Book):
+  book_dict = book.dict()
+  # create id to new data
+  book_dict['id'] = max(data, key=lambda x: x['id']).get('id') + 1
+  data.append(book_dict)
+  return book_dict
+
+
+
+# MODIFY BOOK
+@app.get('/api/v1/books/{book_id}', response_model=Union[Book, str])
+def books(book_id: int, updated_book: Book, response: Response):
+  book = None
+  for b in data:
+    if b['id'] == book_id:
+      book = b
+      break
+
+  if book is None:
+    response.status_code = 404
+    return "Book not found"
+  
+  for key, value in updated_book.dict().items():
+    if key != 'id':  # to not change the id
+      book[key] = value
+  return book
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.orm import relationship
 
-from .database import Base
+# from .database import Base
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
-
-class Book(Base):
-  __tablename__ = "books"
-
-  id = Column(Integer, primary_key=True, index=True)
-  name = Column(String, unique=True)
-  author = Column(String)
-  synopsis = Column(String)
+class Book(BaseModel):
+  id: Optional[int] = None
+  title: str
+  synopsis: str
+  author: str
